8dd6aed8c38805d791ece0dee35da3809d40df24
AVespaIsNotAMotorcycle <samuelmebersole@gmail.com>
2021-01-12 13:37:25 -0500

    ?

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.get('/callback', function(req, res) {
           console.log(body);
         });
 
-        spotifyApi.setAccessToken(access_token);
+        spotifyApi.access_token = access_token;
 
         // we can also pass the token to the browser to make requests from there
         res.redirect('/#' +

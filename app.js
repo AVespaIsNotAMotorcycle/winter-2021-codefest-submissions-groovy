2f1081b011d062275b26531c7641c844a58aafbe
AVespaIsNotAMotorcycle <samuelmebersole@gmail.com>
2021-01-13 15:06:35 -0500

    Posting songs to playlist

--- app.js
+++ app.js
@@ -214,6 +214,7 @@ app.get('/recs', function(req, res){
           }
         }
         request.post(s_options, function(error, response, body) {
+          console.log(s_options);
           console.log(body);
         });
       }

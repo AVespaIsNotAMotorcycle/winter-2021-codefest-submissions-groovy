c977b28f6f363dcab8e1b45e984ef17db2c517e2
AVespaIsNotAMotorcycle <samuelmebersole@gmail.com>
2021-01-12 23:07:42 -0500

    Working on playlist creation

--- app.js
+++ app.js
@@ -202,6 +202,7 @@ app.get('/recs', function(req, res){
   };
 
   request.post(options, function(error, response, body) {
+    console.log('Playlist:' + response.statusCode);
     if (!error && response.statusCode === 200) {
       console.log('success');
     }

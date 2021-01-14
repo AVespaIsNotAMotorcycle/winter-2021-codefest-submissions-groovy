c546e10d0596847e75f124fcac7b016b49c007c3
AVespaIsNotAMotorcycle <40724632+AVespaIsNotAMotorcycle@users.noreply.github.com>
2021-01-13 20:29:36 -0500

    Fetching top artists

--- app.js
+++ app.js
@@ -176,6 +176,18 @@ app.get('/recs', function(req, res){
   });
 
   // Get top tracks
+  var t_tracks = "";
+  var top_tracks_options = {
+    url: 'https://api.spotify.com/v1/me/top/tracks',
+    headers: {
+      'Authorization': 'Bearer ' + spotifyApi.access_token
+    }
+  }
+  request.get(top_artists_options, function(error, response, body) {
+    //console.log(response);
+    t_tracks = JSON.parse(body);
+    console.log(t_tracks);
+  });
 
   // Data & Options for Spotify recommendations request
   var data = querystring.stringify({

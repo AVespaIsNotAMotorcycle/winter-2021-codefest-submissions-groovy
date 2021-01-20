17c253d15c6e27d56b6084d7a6397c7b9128e800
AVespaIsNotAMotorcycle <40724632+AVespaIsNotAMotorcycle@users.noreply.github.com>
2021-01-20 14:20:06 -0500

    Working on async implementation

--- groovy-spotify-module.js
+++ groovy-spotify-module.js
@@ -166,7 +166,7 @@ exports.createGroovyPlaylist = async function (userID, accessToken) {
                 "description": "New playlist description",
                 "public": false
             };
-            let playlist = module.exports.createPlaylist(playlistInfo, userID, accessToken);
+            let playlist = module.exports.createPlaylist(JSON.parse(playlistInfo), userID, accessToken);
 
             playlist.then((res) => {
                 console.log("Created playlist");
